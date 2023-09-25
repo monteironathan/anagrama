a4e644482bd0d5f08353b9b4f776aecbc9a3cd1f
Nathan Monteiro <monteironathann@gmail.com>
2023-09-25 09:44:14 -0300

    commit inicial

---
+++ src/main/java/com/nathan/anagrama/AnagramaApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.anagrama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathan/anagrama/AnagramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.anagrama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
